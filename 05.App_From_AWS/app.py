794192ea49a9d2e23214f1cb0fa22b919ef99c66
ashis.sng <ashis.sng@gmail.com>
2022-01-20 10:20:32 +0000

    updated aws app

--- 05.App_From_AWS/app.py
+++ 05.App_From_AWS/app.py
@@ -3,5 +3,8 @@ app = Flask(__name__)
 @app.route("/welcome")
 def hello():
 	return "Welcome to Ashis Python Project!"
+@app.route("/about")
+def hello():
+	return "This is my sample Python Project running from AWS EC2! Feel free to go to Welcome Page : http://13.127.26.92/welcome"
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80)
