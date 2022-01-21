6fbf6a4e736a98d570252db0e50d8021a55458c4
ashis.sng <ashis.sng@gmail.com>
2022-01-21 11:50:27 +0000

    terraform

---
+++ 07.Terraform/01.CreateBucket.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    access_key = "AKIAZMNCSZ3T4CNWXOFT"
+    secret_key = "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"
+    region = "us-east-1"
+}
+
+
+
+resource "aws_s3_bucket" "createbucketblock" {
+    bucket = "bkttfv1mujahed"
+    tags ={
+        Name="bkttfv1mujahed"
+        Enviornment="Dev"
+    }
+}
